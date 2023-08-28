0d2c299b855efff963774a033670f297a9d7e0e7
Chasen Bettinger <bettingerchasen@gmail.com>
2023-08-28 10:34:02 -0400

    Resolve linting errors
    Signed-off-by: Chasen Bettinger <bettingerchasen@gmail.com>

--- test/getRequestOptions.test.mjs
+++ test/getRequestOptions.test.mjs
@@ -1,22 +1,22 @@
 import assert from 'assert';
 import { getRequestOptions } from '../lib/translation/getRequestOptions.cjs';
-import { DEFAULT_OPTIONS } from "../lib/defaults.cjs"
+import { DEFAULT_OPTIONS } from '../lib/defaults.cjs';
 
 describe('getRequestOptions()', function () {
 
 	it('should result in default options', async () => {        
         const userDefinedOptions = {
-            "headers": {}
-        }
+            'headers': {}
+        };
 
-		const res = getRequestOptions(userDefinedOptions, DEFAULT_OPTIONS.requestOptions, false)
-        assert.deepEqual(res, DEFAULT_OPTIONS.requestOptions)
-    })
+		const res = getRequestOptions(userDefinedOptions, DEFAULT_OPTIONS.requestOptions, false);
+        assert.deepEqual(res, DEFAULT_OPTIONS.requestOptions);
+    });
 	it('should result in user-supplied options', async () => {
         const userDefinedOptions = {
-            "headers": {}
-        }
-		const res = getRequestOptions(userDefinedOptions, DEFAULT_OPTIONS.requestOptions, true)
-        assert.deepEqual(res, userDefinedOptions)
-    })
+            'headers': {}
+        };
+		const res = getRequestOptions(userDefinedOptions, DEFAULT_OPTIONS.requestOptions, true);
+        assert.deepEqual(res, userDefinedOptions);
+    });
 });
